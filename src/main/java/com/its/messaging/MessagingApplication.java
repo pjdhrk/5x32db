7ed0411d491c558449732a934d0366142b99faf0
P. J. Dluzniewski <pjdhrk@gmail.com>
2018-02-20 00:55:34 +0100

    Initial commit

--- src/main/java/com/its/messaging/MessagingApplication.java
+++ src/main/java/com/its/messaging/MessagingApplication.java
@@ -1,7 +1,12 @@
 package com.its.messaging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class MessagingApplication {
@@ -9,4 +14,25 @@ public class MessagingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
+
+}
+
+@RestController
+@Slf4j
+class MessagingController {
+
+	@PostMapping("/send/{type}")
+	public Mono<String> send(@PathVariable String type, @RequestBody(required = false) String contents) {
+		return Mono.empty();
+	}
+
+	@GetMapping(value = "/list", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Message> list() {
+		log.info("I'm here!");
+		return Flux.empty();
+	}
+}
+
+interface Message {
+
 }
--- src/test/java/com/its/messaging/MessagingApplicationTests.java
+++ src/test/java/com/its/messaging/MessagingApplicationTests.java
@@ -1,16 +1,47 @@
 package com.its.messaging;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MessagingApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	WebTestClient client;
+
+	@Before
+	public void setUp() {
+		client = WebTestClient
+				.bindToApplicationContext(applicationContext)
+				.configureClient()
+				.baseUrl("http://localhost:8080/")
+				.build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void shouldGetOkStatusForList() {
+		client.get().uri("/list")
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
+
+	@Test
+	public void shouldGetOkStatusForSend() {
+		client.post().uri("/send/sms")
+
+				.exchange()
+				.expectStatus()
+				.isOk();
 	}
 
 }
